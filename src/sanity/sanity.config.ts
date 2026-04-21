997b88bd351b8006d3aef48b65d215e123f383b6
Genoveva <transadvmarketing@gmail.com>
2026-04-21 14:43:40 -0300

    Dash Destacados

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'sanity';
-import { deskTool } from 'sanity/desk';
+import { deskTool, StructureBuilder } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schema } from './schemas';
 
@@ -9,5 +9,27 @@ export default defineConfig({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   title: 'Trans Advanced Technologies CMS',
   schema,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      structure: (S: StructureBuilder) =>
+        S.list()
+          .title('Contenido')
+          .items([
+            S.listItem()
+              .title('Casos Destacados')
+              .id('featured')
+              .schemaType('project')
+              .child(
+                S.documentList()
+                  .schemaType('project')
+                  .title('Casos Destacados')
+                  .filter('_type == "project" && isHeroFeatured == true')
+                  .defaultOrdering([{ field: 'heroOrder', direction: 'asc' }])
+              ),
+            S.divider(),
+            ...S.documentTypeListItems(),
+          ]),
+    }),
+    visionTool(),
+  ],
 });
